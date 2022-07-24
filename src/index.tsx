bad6188e81ba46ff8e5885d22c1d0a351be3887e
Jordan Waters <jordan@vizibl.co>
2022-07-24 21:23:30 +0200

    initial commit with basic react,ts, webpack env

---
+++ src/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+const App = () => (
+	<h1>Testin react big boiiii </h1>
+);
+
+ReactDom.render(
+	<App />,
+	document.getElementById("root")
+);
