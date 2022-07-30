3d7458fd31ec92289705363fa75f183336c3ee11
Jordan Waters <jordan@vizibl.co>
2022-07-30 17:52:50 +0200

    prod enviroment set up for react in docker, potential issue with npm i step in Dockerfile.prod but to be fixed when there is better internet

---
+++ README.md
@@ -0,0 +1,7 @@
+## Docker react container
+
+#To run dev
+
+To rebuild the image run `docker build -t react-container:dev`
+
+To run the app, run `docker-compose up --build`
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDom from "react-dom";
 import "./index.css";
 const App = () => (
 	<div className="container">
-		<p>an itemmmmmm</p>
+		<p>an itemmmmmm updated</p>
 		<div onClick={() => console.log("something")} >A button</div>
 	</div>
 );
