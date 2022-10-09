9534f5fd0ea85fdc126193e71f0ee2786da3a2c3
Jordan Waters <jordan@vizibl.co>
2022-10-09 14:24:00 +0200

    change port forwarding for dokcer run

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import Plugin from './containers';
 
 const App = () => <Plugin></Plugin>;
-ReactDom.render(<App />, document.getElementById('root'));
+
+const container = document.getElementById('root')
+const root = createRoot(container!)
+root.render(<App />)
